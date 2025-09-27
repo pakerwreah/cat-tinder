e3953babe87a5c70711b5a7084ceaf3a892f62a2
Carlos Enumo <paker_wreah@hotmail.com>
2025-09-28 00:42:16 +0100

    Create "LIKE" and "NOPE" stamps

--- src/components/CatImageCard.tsx
+++ src/components/CatImageCard.tsx
@@ -1,7 +1,7 @@
 import { Image } from 'expo-image';
-import { type ViewProps } from 'react-native';
+import { Text, type ViewProps } from 'react-native';
 import { type CatImage } from '@/lib/cats/types';
-
+import { useCallback } from 'react';
 import { Dimensions } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
@@ -28,33 +28,59 @@ export function CatImageCard({ image, onSwipe, ...props }: Props) {
       translateX.value = e.translationX;
     })
     .onEnd(() => {
-      const shouldDismiss = Math.abs(translateX.value) > SWIPE_THRESHOLD;
-      if (shouldDismiss) {
-        translateX.value = withTiming(
-          (translateX.value > 0 ? width : -width) * 1.5,
-          { duration: 200 },
-          () => {
-            runOnJS(onSwipe)(image.id, translateX.value > 0 ? 'right' : 'left');
-          },
-        );
-      } else {
-        translateX.value = withSpring(0);
+      const shouldSwipe = Math.abs(translateX.value) > SWIPE_THRESHOLD;
+      if (!shouldSwipe) {
+        translateX.value = withSpring(0, { duration: 1000 });
+        return;
       }
+      translateX.value = withTiming(
+        (translateX.value > 0 ? width : -width) * 1.5,
+        { duration: 200 },
+        () => {
+          runOnJS(onSwipe)(image.id, translateX.value > 0 ? 'right' : 'left');
+        },
+      );
     });
 
-  const animatedStyle = useAnimatedStyle(() => {
+  const animatedCardStyle = useAnimatedStyle(() => {
     const rotate = (translateX.value / width) * 20; // max 20deg tilt
     return {
       transform: [{ translateX: translateX.value }, { rotate: `${rotate}deg` }],
     };
   });
 
-  // TODO: show "NOPE" | "LIKE" stamp during gesture
+  const getStampOpacity = useCallback((value: number, side: 'left' | 'right') => {
+    'worklet';
+    const leeway = 0.2; // this is to avoid showing the opposite stamp when springing back
+    return ((side === 'left' ? -1 : 1) * value) / (width / 3) - leeway;
+  }, []);
+
+  const animatedNopeStyle = useAnimatedStyle(() => ({
+    opacity: getStampOpacity(translateX.value, 'left'),
+  }));
+
+  const animatedLikeStyle = useAnimatedStyle(() => ({
+    opacity: getStampOpacity(translateX.value, 'right'),
+  }));
 
   return (
     <GestureDetector gesture={gesture}>
-      <Animated.View {...props} style={animatedStyle}>
+      <Animated.View {...props} style={animatedCardStyle}>
         <Image className="size-full overflow-hidden rounded-3xl bg-gray-400" source={image.url} />
+
+        <Animated.View
+          className="absolute left-8 top-16 rotate-[-20deg] rounded-lg border-4 border-green-500 px-4"
+          style={animatedLikeStyle}
+        >
+          <Text className="text-center font-bold text-4xl leading-snug text-green-500">LIKE</Text>
+        </Animated.View>
+
+        <Animated.View
+          className="absolute right-8 top-16 rotate-[20deg] rounded-lg border-4 border-red-500 px-3"
+          style={animatedNopeStyle}
+        >
+          <Text className="text-center font-bold text-4xl leading-snug text-red-500">NOPE</Text>
+        </Animated.View>
       </Animated.View>
     </GestureDetector>
   );
