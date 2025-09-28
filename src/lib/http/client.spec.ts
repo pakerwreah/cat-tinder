42343cc37b1e98d65d68efe27433b3a31fb061bf
Carlos Enumo <paker_wreah@hotmail.com>
2025-09-28 21:01:45 +0100

    Add more http client tests

--- src/lib/http/client.spec.ts
+++ src/lib/http/client.spec.ts
@@ -1,4 +1,4 @@
-import { BasicHttpClient } from '@/lib/http/client';
+import { BasicHttpClient, type HttpClient } from '@/lib/http/client';
 
 describe('BasicHttpClient', () => {
   const testParams = () => ({
@@ -10,12 +10,17 @@ describe('BasicHttpClient', () => {
     p6: false,
   });
 
-  it('GET method should serialize params in url', async () => {
-    const fetch = jest.fn();
-    const httpClient = jest.mocked(
+  let fetch: jest.Mock;
+  let httpClient: jest.Mocked<HttpClient>;
+
+  beforeEach(() => {
+    fetch = jest.fn();
+    httpClient = jest.mocked(
       new BasicHttpClient('https://base-url.com', { 'x-custom': 'test' }, { fetch }),
     );
+  });
 
+  it('GET method should serialize params in url', async () => {
     await httpClient.fetch('GET', '/path', testParams());
 
     expect(fetch).toHaveBeenCalledWith(
@@ -33,11 +38,6 @@ describe('BasicHttpClient', () => {
   });
 
   it('POST method should serialize params in body as json', async () => {
-    const fetch = jest.fn();
-    const httpClient = jest.mocked(
-      new BasicHttpClient('https://base-url.com', { 'x-custom': 'test' }, { fetch }),
-    );
-
     await httpClient.fetch('POST', '/path', testParams());
 
     expect(fetch).toHaveBeenCalledWith('https://base-url.com/path', {
@@ -49,5 +49,51 @@ describe('BasicHttpClient', () => {
         'x-custom': 'test',
       },
     });
+  });
+
+  it('Successful request should not return error', async () => {
+    fetch.mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve({}),
+    });
+
+    const [error, result] = await httpClient.fetch('GET', '/path');
+
+    expect(error).toBeNull();
+    expect(result).toEqual({});
+  });
+
+  it('Failed json parsing should return error', async () => {
+    fetch.mockResolvedValue({
+      ok: true,
+      json: () => Promise.reject(new Error('Parsing failed')),
+    });
+
+    const [error, result] = await httpClient.fetch('GET', '/path');
+
+    expect(error?.message).toBe('Parsing failed');
+    expect(result).toBeNull();
+  });
+
+  it('Failed request should return error', async () => {
+    fetch.mockRejectedValue(new Error('Request failed'));
+
+    const [error, result] = await httpClient.fetch('GET', '/path');
+
+    expect(error?.message).toBe('Request failed');
+    expect(result).toBeNull();
+  });
+
+  it('Response error should return error', async () => {
+    fetch.mockResolvedValue({
+      ok: false,
+      status: 404,
+      statusText: 'Not Found',
+    });
+
+    const [error, result] = await httpClient.fetch('GET', '/path');
+
+    expect(error?.message).toBe('404 - Not Found');
+    expect(result).toBeNull();
   });
 });
